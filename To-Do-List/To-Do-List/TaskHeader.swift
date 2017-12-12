91ca8989385a33aeabd093a5a10b61ff58637f13
Kylta <Christophe.bugnon02@gmail.com>
2017-12-12 15:08:10 +0100

    Add button for add task in TaskHeader

--- To-Do-List/To-Do-List/TaskHeader.swift
+++ To-Do-List/To-Do-List/TaskHeader.swift
@@ -22,12 +22,22 @@ class TaskHeader: BaseCell {
         return textField
     }()
     
+    let addTaskButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add Task", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func setupViews() {
         
         addSubview(taskNameTextField)
-        // Constraint for cells
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: [], metrics: nil, views: ["v0": taskNameTextField]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: [], metrics: nil, views: ["v0": taskNameTextField]))
+        addSubview(addTaskButton)
+        
+        // Constraint for cells + header
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-[v1(80)]-8-|", options: [], metrics: nil, views: ["v0": taskNameTextField, "v1": addTaskButton]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-24-[v0]-24-|", options: [], metrics: nil, views: ["v0": taskNameTextField]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: [], metrics: nil, views: ["v0": addTaskButton]))
         
     }
 }
