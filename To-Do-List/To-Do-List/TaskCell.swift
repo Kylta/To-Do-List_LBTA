a08ccd1df2bfbdf2028520b47d50e610cb855917
Kylta <Christophe.bugnon02@gmail.com>
2017-12-12 13:52:45 +0100

    Create nameLabel in TaskCall class + add constraints

--- To-Do-List/To-Do-List/TaskCell.swift
+++ To-Do-List/To-Do-List/TaskCell.swift
@@ -19,9 +19,20 @@ class TaskCell: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    // Name for item in cells
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sample Task"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     func setupViews() {
+        
+        addSubview(nameLabel)
+        // Constraint for cells
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: [], metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: [], metrics: nil, views: ["v0": nameLabel]))
         
     }
 }
