2c68f1050d268275b47389a1ad818005cf9d218d
Kylta <Christophe.bugnon02@gmail.com>
2017-12-12 13:40:20 +0100

    Choose the number of item + create identifier with index. Use register on collectionView with new class call TaskCell with identifier call cellId

---
+++ To-Do-List/To-Do-List/TaskCell.swift
@@ -0,0 +1,27 @@
+//
+//  TaskCell.swift
+//  To-do-List
+//
+//  Created by Christophe Bugnon on 12/12/2017.
+//  Copyright © 2017 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+// Class create for use on CollectionView register
+class TaskCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setupViews() {
+        
+    }
+}
--- To-Do-List/To-Do-List/ViewController.swift
+++ To-Do-List/To-Do-List/ViewController.swift
@@ -14,9 +14,24 @@ class CollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         navigationItem.title = "To-Do List"
-        
+        collectionView?.backgroundColor = .white
+        // For scroll cells on vertical
+        collectionView?.alwaysBounceVertical =  true
+        // Create class for cells + use cell identifier
+        collectionView?.register(TaskCell.self, forCellWithReuseIdentifier: "cellId")
         
     }
-
+    
+    // Number of cells
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    // Index for cells
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // method for cell had a name + index at indexPath
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+    }
+    
 }
 
